c874083955240ead87cb60c8f44bfae9d2a278e2
ALDY <142135012+ZackRyu12@users.noreply.github.com>
2024-10-10 22:09:15 +0700

    Delete order_detail.sql

--- order_detail.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE order_detail (
-    order_detail_id SERIAL PRIMARY KEY,
-    order_id INT NOT NULL ,
-    service_id INT NOT NULL,
-    qty INT NOT NULL,
-    FOREIGN KEY(order_id) REFERENCES "order"(order_id),
-    FOREIGN KEY(service_id) REFERENCES service(service_id)
-);
